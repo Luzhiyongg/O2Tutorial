c1591b86d1b4d9357a48146a300d3d61ca0cf991
Luzhiyongg <71517277+Luzhiyongg@users.noreply.github.com>
2023-11-09 17:01:09 +0800

    fix run.sh and first run

--- GenericFramework/run.sh
+++ GenericFramework/run.sh
@@ -10,21 +10,19 @@ if [ "$run" = "run3" ]; then
     o2-analysis-track-propagation ${opt} |
     o2-analysis-tracks-extra-converter ${opt} |
     o2-analysis-timestamp ${opt} | 
-    o2-analysis-timestamp ${opt} | 
     o2-analysis-bc-converter ${opt} |
     o2-analysis-multiplicity-table ${opt} | 
     o2-analysis-trackselection ${opt} | 
     o2-analysis-centrality-table ${opt} |
     o2-analysis-event-selection ${opt} | 
-    o2-analysis-pid-tpc-full ${opt} | o2-analysis-pid-tpc-base ${opt} | 
-    o2-analysis-pid-tof-full ${opt} | o2-analysis-pid-tof-base ${opt} | 
-    o2-analysis-pid-bayes ${opt} |
     o2-analysis-cf-flow-gfw-tutorial ${opt}  --aod-file ${aodfile} --aod-memory-rate-limit 429496320
 else 
     opt="-b --configuration json://configuration.json"
     aodfile="AO2D_LHC18q_converted.root"
+    o2-analysis-trackextension ${opt} |
     o2-analysis-tracks-extra-converter ${opt} |
     o2-analysis-bc-converter ${opt} |
+    o2-analysis-timestamp ${opt} | 
     o2-analysis-collision-converter ${opt} |
     o2-analysis-zdc-converter ${opt} |
     o2-analysis-multiplicity-table ${opt} | 
