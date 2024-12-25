e4a6f9e82dd9142d99720b854c1fba6d6dd8064b
Luzhiyongg <71517277+Luzhiyongg@users.noreply.github.com>
2024-12-26 00:11:29 +0800

    update local run script

--- MyRun3Analysis/run_FlowRunbyRun.sh
+++ MyRun3Analysis/run_FlowRunbyRun.sh
@@ -1,9 +1,10 @@
 opt="-b --configuration json://configuration_flow_runbyrun.json"
-    aodfile="AO2D_LHC23zzh_apass4_544122.root"   
+    aodfile="AO2D_LHC23zzm_apass4_544652.root"   
     o2-analysis-track-propagation ${opt} |
+    o2-analysis-tracks-extra-v002-converter ${opt} | 
     o2-analysis-timestamp ${opt} | 
     o2-analysis-multiplicity-table ${opt} | 
     o2-analysis-trackselection ${opt} | 
     o2-analysis-centrality-table ${opt} |
     o2-analysis-event-selection ${opt} | 
-    o2-analysis-cf-flow-runbyrun ${opt}  --aod-file ${aodfile} --aod-memory-rate-limit 429496320
+    o2-analysis-cf-flow-runby-run ${opt}  --aod-file ${aodfile} --aod-memory-rate-limit 429496320
