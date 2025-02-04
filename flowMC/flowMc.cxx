08536496967359061dd1eaf044e7caa87c9832e3
Luzhiyongg <71517277+Luzhiyongg@users.noreply.github.com>
2025-02-05 02:03:52 +0800

    o2_linter

--- flowMC/flowMc.cxx
+++ flowMC/flowMc.cxx
@@ -9,9 +9,10 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-// flow test for QC of synthetic flow exercise
-// cross-PWG effort in tracking studies
-// includes basic tracking, V0s and Cascades
+/// \file   flowMc.cxx
+/// \author Zhiyong Lu (zhiyong.lu@cern.ch)
+/// \since  Feb/5/2025
+/// \brief  QC of synthetic flow exercise
 
 #include "CCDB/BasicCCDBManager.h"
 #include "Framework/AnalysisDataModel.h"
@@ -40,7 +41,7 @@ using namespace o2::framework::expressions;
 
 #define O2_DEFINE_CONFIGURABLE(NAME, TYPE, DEFAULT, HELP) Configurable<TYPE> NAME{#NAME, DEFAULT, HELP};
 
-struct flowMc {
+struct FlowMc {
   HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
   Configurable<float> minB{"minB", 0.0f, "min impact parameter"};
@@ -52,7 +53,7 @@ struct flowMc {
   O2_DEFINE_CONFIGURABLE(cfgFlowEfficiency, std::string, "", "CCDB path to efficiency object")
 
   ConfigurableAxis axisB{"axisB", {100, 0.0f, 20.0f}, ""};
-  ConfigurableAxis axisPhi{"axisPhi", {100, 0.0f, 2.0f * TMath::Pi()}, ""};
+  ConfigurableAxis axisPhi{"axisPhi", {100, 0.0f, constants::math::TwoPI}, ""};
   ConfigurableAxis axisNch{"axisNch", {300, 0.0f, 3000.0f}, "Nch in |eta|<0.8"};
 
   ConfigurableAxis axisPt{"axisPt", {VARIABLE_WIDTH, 0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 1.1f, 1.2f, 1.3f, 1.4f, 1.5f, 1.6f, 1.7f, 1.8f, 1.9f, 2.0f, 2.2f, 2.4f, 2.6f, 2.8f, 3.0f, 3.2f, 3.4f, 3.6f, 3.8f, 4.0f, 4.4f, 4.8f, 5.2f, 5.6f, 6.0f, 6.5f, 7.0f, 7.5f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f}, "pt axis"};
@@ -142,16 +143,16 @@ struct flowMc {
     return true;
   }
 
-  using recoTracks = soa::Join<aod::TracksIU, aod::TracksExtra>;
+  using RecoTracks = soa::Join<aod::TracksIU, aod::TracksExtra>;
 
-  void process(aod::McCollision const& mcCollision, soa::Join<aod::McParticles, aod::ParticlesToTracks> const& mcParticles, recoTracks const&)
+  void process(aod::McCollision const& mcCollision, soa::Join<aod::McParticles, aod::ParticlesToTracks> const& mcParticles, RecoTracks const&)
   {
 
     float imp = mcCollision.impactParameter();
     float evPhi = mcCollision.eventPlaneAngle();
     float vtxz = mcCollision.posZ();
     if (evPhi < 0)
-      evPhi += 2. * TMath::Pi();
+      evPhi += constants::math::TwoPI;
 
     long nCh = 0;
     float weff = 1.;
@@ -166,20 +167,20 @@ struct flowMc {
 
       for (auto const& mcParticle : mcParticles) {
         // focus on bulk: e, mu, pi, k, p
-        int pdgCode = TMath::Abs(mcParticle.pdgCode());
+        int pdgCode = std::abs(mcParticle.pdgCode());
         if (pdgCode != 11 && pdgCode != 13 && pdgCode != 211 && pdgCode != 321 && pdgCode != 2212)
           continue;
 
         if (!mcParticle.isPhysicalPrimary())
           continue;
-        if (TMath::Abs(mcParticle.eta()) > 0.8) // main acceptance
+        if (std::fabs(mcParticle.eta()) > 0.8) // main acceptance
           continue;
 
         float deltaPhi = mcParticle.phi() - mcCollision.eventPlaneAngle();
         if (deltaPhi < 0)
-          deltaPhi += 2. * TMath::Pi();
-        if (deltaPhi > 2. * TMath::Pi())
-          deltaPhi -= 2. * TMath::Pi();
+          deltaPhi += constants::math::TwoPI;
+        if (deltaPhi > constants::math::TwoPI)
+          deltaPhi -= constants::math::TwoPI;
         histos.fill(HIST("hPtVsPhiGenerated"), deltaPhi, mcParticle.pt());
         histos.fill(HIST("hBVsPtVsPhiGenerated"), imp, deltaPhi, mcParticle.pt());
 
@@ -191,7 +192,7 @@ struct flowMc {
         bool validITSTrack = false;
         bool validITSABTrack = false;
         if (mcParticle.has_tracks()) {
-          auto const& tracks = mcParticle.tracks_as<recoTracks>();
+          auto const& tracks = mcParticle.tracks_as<RecoTracks>();
           for (auto const& track : tracks) {
             if (track.hasTPC() && track.hasITS()) {
               validGlobal = true;
@@ -238,25 +239,25 @@ struct flowMc {
 
   using LabeledCascades = soa::Join<aod::CascDataExt, aod::McCascLabels>;
 
-  void processCascade(aod::McParticle const& mcParticle, soa::SmallGroups<LabeledCascades> const& cascades, recoTracks const&, aod::McCollisions const&)
+  void processCascade(aod::McParticle const& mcParticle, soa::SmallGroups<LabeledCascades> const& cascades, RecoTracks const&, aod::McCollisions const&)
   {
     auto mcCollision = mcParticle.mcCollision();
     float imp = mcCollision.impactParameter();
 
-    int pdgCode = TMath::Abs(mcParticle.pdgCode());
+    int pdgCode = std:abs(mcParticle.pdgCode());
     if (pdgCode != 3312 && pdgCode != 3334)
       return;
 
     if (!mcParticle.isPhysicalPrimary())
       return;
-    if (TMath::Abs(mcParticle.eta()) > 0.8)
+    if (std::fabs(mcParticle.eta()) > 0.8)
       return;
 
     float deltaPhi = mcParticle.phi() - mcCollision.eventPlaneAngle();
     if (deltaPhi < 0)
-      deltaPhi += 2. * TMath::Pi();
-    if (deltaPhi > 2. * TMath::Pi())
-      deltaPhi -= 2. * TMath::Pi();
+      deltaPhi += constants::math::TwoPI;
+    if (deltaPhi > constants::math::TwoPI)
+      deltaPhi -= constants::math::TwoPI;
     if (pdgCode == 3312)
       histos.fill(HIST("hBVsPtVsPhiGeneratedXi"), imp, deltaPhi, mcParticle.pt());
     if (pdgCode == 3334)
@@ -269,29 +270,29 @@ struct flowMc {
         histos.fill(HIST("hBVsPtVsPhiGlobalOmega"), imp, deltaPhi, mcParticle.pt());
     }
   }
-  PROCESS_SWITCH(flowMc, processCascade, "Process cascades", true);
+  PROCESS_SWITCH(FlowMc, processCascade, "Process cascades", true);
 
   using LabeledV0s = soa::Join<aod::V0Datas, aod::McV0Labels>;
 
-  void processV0s(aod::McParticle const& mcParticle, soa::SmallGroups<LabeledV0s> const& v0s, recoTracks const&, aod::McCollisions const&)
+  void processV0s(aod::McParticle const& mcParticle, soa::SmallGroups<LabeledV0s> const& v0s, RecoTracks const&, aod::McCollisions const&)
   {
     auto mcCollision = mcParticle.mcCollision();
     float imp = mcCollision.impactParameter();
 
-    int pdgCode = TMath::Abs(mcParticle.pdgCode());
+    int pdgCode = std::abs(mcParticle.pdgCode());
     if (pdgCode != 310 && pdgCode != 3122)
       return;
 
     if (!mcParticle.isPhysicalPrimary())
       return;
-    if (TMath::Abs(mcParticle.eta()) > 0.8)
+    if (std::fabs(mcParticle.eta()) > 0.8)
       return;
 
     float deltaPhi = mcParticle.phi() - mcCollision.eventPlaneAngle();
     if (deltaPhi < 0)
-      deltaPhi += 2. * TMath::Pi();
-    if (deltaPhi > 2. * TMath::Pi())
-      deltaPhi -= 2. * TMath::Pi();
+      deltaPhi += constants::math::TwoPI;
+    if (deltaPhi > constants::math::TwoPI)
+      deltaPhi -= constants::math::TwoPI;
     if (pdgCode == 310)
       histos.fill(HIST("hBVsPtVsPhiGeneratedK0Short"), imp, deltaPhi, mcParticle.pt());
     if (pdgCode == 3122)
@@ -304,11 +305,11 @@ struct flowMc {
         histos.fill(HIST("hBVsPtVsPhiGlobalLambda"), imp, deltaPhi, mcParticle.pt());
     }
   }
-  PROCESS_SWITCH(flowMc, processV0s, "Process V0s", true);
+  PROCESS_SWITCH(FlowMc, processV0s, "Process V0s", true);
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<flowMc>(cfgc)};
+    adaptAnalysisTask<FlowMc>(cfgc)};
 }
